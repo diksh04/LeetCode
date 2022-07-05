28a90f91b1b8cf02f3a64aecf044614a4aa32dd6
diksh04 <84238934+diksh04@users.noreply.github.com>
2022-07-05 16:02:39 +0530

    Time: 20 ms (43.85%), Space: 8.1 MB (36.92%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char,int>roman;
+        roman.insert(make_pair('I',1));
+        roman.insert(make_pair('V',5));
+        roman.insert(make_pair('X',10));
+        roman.insert(make_pair('L',50));
+        roman.insert(make_pair('C',100));
+        roman.insert(make_pair('D',500));
+        roman.insert(make_pair('M',1000));
+        int sum = 0,num=0;
+        for(int i=0;i<s.size();)
+        {
+            if(i==s.size()-1 || roman[s[i]]>=roman[s[i+1]])
+            {
+                num=roman[s[i]];
+                i++;
+            }
+            else
+            {
+                num = roman[s[i+1]]-roman[s[i]];
+                i+=2;
+            }
+            sum+=num;
+        }
+        return sum;
+    }
+};
