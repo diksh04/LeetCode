cf07b0b190fbc133dd880e173ff3b66596b8a766
diksh04 <84238934+diksh04@users.noreply.github.com>
2022-10-17 00:33:58 +0530

    Time: 84 ms (52.87%), Space: 8.3 MB (18.79%) - LeetHub

---
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    
+    int helpme(vector<int>&nums,int d,int idx,int &n,vector<vector<int>>&dp,vector<int>&maxarr)
+    {
+        if(n<d)
+            return -1;
+        if(d==1)
+            return maxarr[idx];
+        if(dp[d][idx]!=-2)
+            return  dp[d][idx];
+        int ans=INT_MAX;
+        int maxTillithIndex=INT_MIN;
+        for(int i=idx;i<=n-d;i++)
+        {
+            maxTillithIndex=max(maxTillithIndex,nums[i]);
+            ans=min(ans,maxTillithIndex+helpme(nums,d-1,i+1,n,dp,maxarr));
+        }
+        return dp[d][idx]=ans;
+    }
+     int minDifficulty(vector<int>& A, int D) {
+       int n=A.size();
+         vector<vector<int>>dp(11,vector<int>(301,-2));
+         
+         //this vector is to store the maxElement in the righwards(i.e. maxarr[i] contains max element from i...n)
+         vector<int>maxarr(n,0);
+         maxarr[n-1]=A[n-1];
+         for(int i=n-2;i>=0;i--)
+             maxarr[i]=max(maxarr[i+1],A[i]);
+         return helpme(A,D,0,n,dp,maxarr);
+    }
+};
