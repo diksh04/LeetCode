ac0608a34b2cfbd56310bc94bdeccea0fb93a708
diksh04 <84238934+diksh04@users.noreply.github.com>
2023-04-08 02:28:33 +0530

    Time: 18 ms (31.24%), Space: 9.5 MB (50.41%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,35 +1,71 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& grid,int i,int j)
+    int dx[4] = {1,-1,0,0};
+    int dy[4] = {0,0,1,-1};
+    void dfs(vector<vector<int>>& grid,int x,int y,int &n,int &m)
     {
-        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size())
+        if(x<0 || y<0 || x>=n || y>=m || grid[x][y]==1)
         {
-            return false;
+            return;
         }
-        if(grid[i][j]==1)
+        grid[x][y]=1;
+        for(int i=0;i<4;i++)
         {
-            return true;
+            dfs(grid,x+dx[i],y+dy[i],n,m);
         }
-        grid[i][j] = 1;
-        bool p1 = dfs(grid,i+1,j);
-        bool p2 = dfs(grid,i,j+1);
-        bool p3 = dfs(grid,i-1,j);
-        bool p4 = dfs(grid,i,j-1);
-        return p1 && p2 && p3 && p4;
     }
     int closedIsland(vector<vector<int>>& grid) {
-        int count = 0;
-        int m = grid.size(),n = grid[0].size();
-        for(int i = 0;i < m;i++)
+        int n = grid.size();
+        int m = grid[0].size();
+        queue<pair<int,int>>q;
+        // top and bottom wall
+        for(int i=0;i<m;i++)
         {
-            for(int j=0;j<n;j++)
+            if(grid[0][i]==0)
+            {
+                q.push({0,i});
+            }
+            if(grid[n-1][i]==0)
+            {
+                q.push({n-1,i});
+            }
+        }
+        //left and right wall
+        for(int i=1;i<n-1;i++)
+        {
+            if(grid[i][0]==0)
+            {
+                q.push({i,0});
+            }
+            if(grid[i][m-1]==0)
+            {
+                q.push({i,m-1});
+            }
+        }
+        while(!q.empty())
+        {
+            pair<int,int>front = q.front();
+            q.pop();
+            int x = front.first;
+            int y = front.second;
+            if(grid[x][y]==1)
+            {
+                continue;
+            }
+            dfs(grid,x,y,n,m);
+        }
+        int countComps = 0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
             {
                 if(grid[i][j]==0)
                 {
-                    count+=dfs(grid,i,j)?1:0;
+                    countComps++;
+                    dfs(grid,i,j,n,m);
                 }
             }
         }
-        return count;
+        return countComps;
     }
 };
