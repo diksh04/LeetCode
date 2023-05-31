8f684cbc1e0a4555edf6bafe1848c9aa6df3cdb9
diksh04 <84238934+diksh04@users.noreply.github.com>
2023-05-31 13:56:33 +0530

    Time: 79 ms (58.03%), Space: 27.5 MB (90.62%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,58 +1,52 @@
 class Solution {
 public:
-    int drow[4] = {-1,0,1,0};
-    int dcol[4] = {0,1,0,-1};
-    void dfs(int r,int c,vector<vector<int>>& vis,vector<vector<int>>& grid)
-    {
-        vis[r][c] = 1;
-        for(int i=0;i<4;i++)
-        {
-            int nrow = r + drow[i];
-            int ncol = c + dcol[i];
-            if(nrow>=0 && nrow<grid.size() && ncol>=0 && ncol<grid[0].size() && !vis[nrow][ncol] && grid[nrow][ncol]==1)
-            {
-                dfs(nrow,ncol,vis,grid);
-            }
-        }
-    }
-    int numEnclaves(vector<vector<int>>& grid) {
+    int numEnclaves(vector<vector<int>>& grid) {//bfs
         int n = grid.size(),m = grid[0].size();
         vector<vector<int>>vis(n,vector<int>(m,0));
-        // top and bottom cell
-        for(int j=0;j<m;j++)
-        {
-            if(!vis[0][j] && grid[0][j]==1)
-            {
-                dfs(0,j,vis,grid);
-            }
-            if(!vis[n-1][j] && grid[n-1][j]==1)
-            {
-                dfs(n-1,j,vis,grid);
-            }
-        }
-        //left and right cell
-        for(int i=0;i<n;i++)
-        {
-            if(!vis[i][0] && grid[i][0]==1)
-            {
-                dfs(i,0,vis,grid);
-            }
-            if(!vis[i][m-1] && grid[i][m-1]==1)
-            {
-                dfs(i,m-1,vis,grid);
-            }
-        }
-        int countLandcell = 0;
+        queue<pair<int,int>>q;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(!vis[i][j] && grid[i][j]==1)
+                if(i==0 || j==0 || i==n-1 || j==m-1)
                 {
-                    countLandcell++;
+                    if(grid[i][j]==1)
+                    {
+                        q.push({i,j});
+                        vis[i][j]=1;
+                    }
                 }
             }
         }
-        return countLandcell;
+        while(!q.empty())
+        {
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+            int drow[4] = {-1,0,1,0};
+            int dcol[4] = {0,1,0,-1};
+            for(int i=0;i<4;i++)
+            {
+                int nrow = r + drow[i];
+                int ncol = c + dcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && grid[nrow][ncol]==1)
+                {
+                    q.push({nrow,ncol});
+                    vis[nrow][ncol]=1;
+                }
+            }
+        }
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==1 && !vis[i][j])
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 };
