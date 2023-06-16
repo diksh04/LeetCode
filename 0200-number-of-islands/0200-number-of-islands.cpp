05276a645b8fc8d59005ad1b1124b1e21391d47d
diksh04 <84238934+diksh04@users.noreply.github.com>
2023-06-16 16:27:53 +0530

    Time: 45 ms (40.53%), Space: 19.1 MB (15.39%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,31 +1,45 @@
 class Solution {
 public:
-    void dfs(int i,int j,vector<vector<char>>& grid)
+    
+    void bfs(int i,int j,vector<vector<int>>& vis,vector<vector<char>>& grid)
     {
-        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() || grid[i][j]=='0')
+        queue<pair<int,int>>q;
+        q.push({i,j});
+        vis[i][j]=1;
+        int dRow[] = {-1,0,1,0};
+        int dCol[] = {0,1,0,-1};
+        while(!q.empty())
         {
-            return;
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+            for(int k=0;k<4;k++)
+            {
+                int nrow = r + dRow[k];
+                int ncol = c + dCol[k];
+                if(nrow>=0 && nrow<grid.size() && ncol>=0 && ncol<grid[0].size() && grid[nrow][ncol]=='1' && !vis[nrow][ncol])
+                {
+                    q.push({nrow,ncol});
+                    vis[nrow][ncol]=1;
+                }
+            }
         }
-        grid[i][j] = '0';
-        dfs(i-1,j,grid);
-        dfs(i,j-1,grid);
-        dfs(i+1,j,grid);
-        dfs(i,j+1,grid);
     }
     int numIslands(vector<vector<char>>& grid) {
+        int countIsl=0;
         int n = grid.size(),m = grid[0].size();
-        int countIslands = 0;
+        vector<vector<int>>vis(n,vector<int>(m,0));
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(grid[i][j]=='1')
+                if(grid[i][j]=='1' && !vis[i][j])
                 {
-                    countIslands++;
-                    dfs(i,j,grid);
+                    countIsl++;
+                    bfs(i,j,vis,grid);
                 }
             }
         }
-        return countIslands;
+        return countIsl;
     }
 };
