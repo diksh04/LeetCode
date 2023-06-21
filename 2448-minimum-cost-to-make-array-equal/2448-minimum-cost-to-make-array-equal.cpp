b3a91b34d490b3d327713bfb92cb08277b5d3e65
diksh04 <84238934+diksh04@users.noreply.github.com>
2023-06-21 21:21:25 +0530

    Time: 101 ms (73.36%), Space: 41.7 MB (42.34%) - LeetHub

---
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    long long helper(vector<int>& nums,vector<int>& cost,long long median)
+    {
+        long long ans = 0;
+        for(int i=0;i<nums.size();i++)
+        {
+            ans = ans + abs(1ll * nums[i]-median)*(1ll*cost[i]);
+        }
+        return ans;
+    }
+    long long minCost(vector<int>& nums, vector<int>& cost) {
+        long long int total = 0,sum = 0;
+        long long n  = cost.size();
+        vector<pair<int,int>>v;
+        for(int i=0;i<n;i++)
+        {
+            v.push_back({nums[i],cost[i]});
+        }
+        // 1-> 2 , 2->14,3->3,5->1
+        //1 1 2 2 2 2 2 2 2 2 2  2  2  2  3  3  3  5  -> sum = 44
+        //0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 
+        sort(v.begin(),v.end());
+        for(int i=0;i<n;i++)//total number of elements
+        {
+            sum = sum + 1ll * v[i].second;
+        }
+        long long median;
+        int i = 0;
+        while(total<(sum+1)/2 && i < n)
+        {
+            total+=1ll * v[i].second;
+            median = v[i].first;
+            i++;
+        }
+        return helper(nums,cost,median);
+    }
+};
