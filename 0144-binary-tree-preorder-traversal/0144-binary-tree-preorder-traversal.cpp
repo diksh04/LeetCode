f2f7f372823035723bd2809e6a3fd3a34c2306a2
diksh04 <84238934+diksh04@users.noreply.github.com>
2023-07-14 22:41:12 +0530

    Time: 3 ms (45.49%), Space: 8.4 MB (63.18%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,19 +11,29 @@
  */
 class Solution {
 public:
-    void helper(TreeNode* root,vector<int>& ans)
-    {
+    vector<int> preorderTraversal(TreeNode* root) {
+        //iterative pre order traversal
+        vector<int>ans;
+        stack<TreeNode*>st;
         if(root==NULL)
         {
-            return;
+            return ans;
         }
-        ans.push_back(root->val);
-        helper(root->left,ans);
-        helper(root->right,ans);
-    }
-    vector<int> preorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        helper(root,ans);
+        st.push(root);
+        while(!st.empty())
+        {
+            root = st.top();
+            st.pop();
+            ans.push_back(root->val);
+            if(root->right)
+            {
+                st.push(root->right);
+            }
+            if(root->left)
+            {
+                st.push(root->left);
+            }
+        }
         return ans;
     }
 };
