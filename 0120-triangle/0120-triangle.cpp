8e739fe256e02892155608c99000d6179d434c89
diksh04 <84238934+diksh04@users.noreply.github.com>
2023-08-24 02:04:04 +0530

    Time: 4 ms (82.67%), Space: 9 MB (21.32%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int helper(int i,int j,vector<vector<int>>& triangle,int n,vector<vector<int>>& dp)
+    {
+        if(i==n-1)
+        {
+            return triangle[n-1][j];
+        }
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+        int down = triangle[i][j] + helper(i+1,j,triangle,n,dp);
+        int up = triangle[i][j] + helper(i+1,j+1,triangle,n,dp);
+        return dp[i][j] = min(down,up);
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        return helper(0,0,triangle,n,dp);
+    }
+};
