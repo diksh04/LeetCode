5e23417115f6832162358623dae1b7db29c4cc80
diksh04 <84238934+diksh04@users.noreply.github.com>
2023-08-25 22:29:39 +0530

    Time: 20 ms (37.13%), Space: 26.9 MB (26.33%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
     int m,n,N;
-    bool helper(int i,int j,int k,string s1,string s2,string s3,vector<vector<vector<int>>>& dp)
+    bool helper(int i,int j,string s1,string s2,string s3,vector<vector<int>>& dp)
     {
-        if(i==m && j==n && k==N)
+        if(i==m && j==n && i+j==N)
         {
             return true;
         }
-        if(k>=N)
+        if(i+j>=N)
         {
             return false;
         }
-        if(dp[i][j][k]!=-1)
+        if(dp[i][j]!=-1)
         {
-            return dp[i][j][k];
+            return dp[i][j];
         }
         bool result = false;
-        if(s1[i]==s3[k])
+        if(s1[i]==s3[i+j])
         {
-            result = helper(i+1,j,k+1,s1,s2,s3,dp);
+            result = helper(i+1,j,s1,s2,s3,dp);
         }
         if(result == true)
         {
-            return dp[i][j][k] = result;
+            return dp[i][j]= result;
         }
-        if(s2[j]==s3[k])
+        if(s2[j]==s3[i+j])
         {
-            result = helper(i,j+1,k+1,s1,s2,s3,dp);
+            result = helper(i,j+1,s1,s2,s3,dp);
         }
-        return dp[i][j][k] = result;
+        return dp[i][j] = result;
     }
     bool isInterleave(string s1, string s2, string s3) {
-//         TC = O(n*m*N)
+//         TC = O(n*m) since k = i+j at every point
         m = s1.size(),n = s2.size(),N = s3.size();
-        vector<vector<vector<int>>>dp(m+1,vector<vector<int>>(n+1,vector<int>(N+1,-1)));
-        return helper(0,0,0,s1,s2,s3,dp);
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        return helper(0,0,s1,s2,s3,dp);
     }
 };
