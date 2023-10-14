95dfc2503e5e68bd95493f3936423be312af4bd7
diksh04 <84238934+diksh04@users.noreply.github.com>
2023-10-14 12:07:50 +0530

    Added solution - LeetHub

--- Cheapest Flights Within K Stops - GFG/cheapest-flights-within-k-stops.cpp
+++ Cheapest Flights Within K Stops - GFG/cheapest-flights-within-k-stops.cpp
@@ -13,25 +13,25 @@ class Solution {
             adj[it[0]].push_back({it[1],it[2]});
         }
         queue<pair<int,pair<int,int>>>q;
-        q.push({0,{src,0}});//stops,node,dist
+        q.push({0,{src,0}});
         vector<int>dist(n,1e9);
         dist[src] = 0;
         while(!q.empty())
         {
             auto it = q.front();
-            q.pop();
             int stops = it.first;
             int node = it.second.first;
             int cost = it.second.second;
+            q.pop();
             if(stops > K) continue;
             for(auto nbr:adj[node])
             {
                 int adjNode = nbr.first;
                 int wt = nbr.second;
-                if(cost + wt < dist[adjNode] && stops<=K)
+                if(wt + cost < dist[adjNode] && stops<=K)
                 {
-                    dist[adjNode] = cost + wt;
-                    q.push({stops+1,{adjNode,dist[adjNode]}});
+                    dist[adjNode] = wt + cost;
+                    q.push({stops+1,{adjNode,wt+cost}});
                 }
             }
         }
