0b4022aa7aa70583f7a89d0461a004a5fd0b1b2b
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-02-17 13:45:22 +0530

    Time: 2 ms (49.65%), Space: 7.1 MB (21.51%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int helper(int n,int k){
+        if(n==1 && k==1){
+            return 0;
+        }
+        int mid = (pow(2,n-1))/2;
+        if(k<=mid){
+            return helper(n-1,k);
+        }
+        return !helper(n-1,k-mid);
+    }
+    int kthGrammar(int n, int k) {
+        return helper(n,k);
+    }
+};
