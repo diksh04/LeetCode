757e0453b6f74d8995181103c8edb480eeb4cded
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-02-19 21:05:16 +0530

    Time: 4 ms (43.75%), Space: 8.8 MB (49.85%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,50 +1,22 @@
 class Solution {
 public:
-    void nextPermu(vector<int>&nums)
+    void helper(int idx,vector<int>& nums,vector<vector<int>>& ans)
     {
-        int i = nums.size()-2;
-        while(i>=0 && nums[i]>=nums[i+1])
+        if(idx==nums.size())
         {
-            i--;
+            ans.push_back(nums);
+            return;
         }
-        if(i>=0)
+        for(int i=idx;i<nums.size();i++)
         {
-            int j = nums.size()-1;
-            while(j>=0 && nums[j]<=nums[i])
-            {
-                j--;
-            }
-            swap(nums[i],nums[j]);
+            swap(nums[i],nums[idx]);
+            helper(idx+1,nums,ans);
+            swap(nums[i],nums[idx]);
         }
-        int st = i+1;
-        int end = nums.size()-1;
-        while(st<=end)
-        {
-            int temp = nums[st];
-            nums[st]=nums[end];
-            nums[end]=temp;
-            st++;
-            end--;
-        }
-    }
-    int fact(int n)
-    {
-        if(n==0 || n==1)
-        {
-            return 1;
-        }
-        return n*fact(n-1);
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        int n = nums.size();
-        int k = fact(n);
         vector<vector<int>>ans;
-        ans.push_back(nums);
-        for(int i=1;i<k;i++)
-        {
-            nextPermu(nums);
-            ans.push_back(nums); 
-        }
+        helper(0,nums,ans);
         return ans;
     }
 };
