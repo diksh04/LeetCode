91d4dfcc898aa165e75b4825151ea42e31724042
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-02-25 00:34:15 +0530

    Time: 3 ms (87.42%), Space: 11.4 MB (32.92%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    int helper(int i,int j,vector<vector<int>>& triangle,int n,vector<vector<int>>& dp)
+    int helper(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>& dp)
     {
-        if(i==n-1)
+        if(i==triangle.size()-1)
         {
-            return triangle[n-1][j];
+            return triangle[i][j];
         }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        int down = triangle[i][j] + helper(i+1,j,triangle,n,dp);
-        int up = triangle[i][j] + helper(i+1,j+1,triangle,n,dp);
-        return dp[i][j] = min(down,up);
+        if(dp[i][j]!=-1) return dp[i][j];
+        int down = triangle[i][j] + helper(i+1,j,triangle,dp);
+        int diag = triangle[i][j] + helper(i+1,j+1,triangle,dp);
+        return dp[i][j] = min(down,diag);
     }
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
+        int n = triangle.size(),m = triangle[0].size();
         vector<vector<int>>dp(n,vector<int>(n,-1));
-        return helper(0,0,triangle,n,dp);
+        return helper(0,0,triangle,dp);
     }
 };
+
+// 2
+// 3 4
+// 6 5 7
+// 4 1 8 3
