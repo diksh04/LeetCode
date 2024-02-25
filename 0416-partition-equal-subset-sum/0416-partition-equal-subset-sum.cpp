c837238bfe938d76823a48910b5a6921b6678c1b
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-02-25 13:58:03 +0530

    Time: 264 ms (44.16%), Space: 12.4 MB (86.35%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
-    bool helper(int idx,int target,vector<int>& nums,vector<vector<int>>& dp)
+    bool subsetSumToK(int n, int k, vector<int> &arr) {
+    // Write your code here.
+    vector<bool>prev(k+1,0),curr(k+1,0);
+    prev[0] = curr[0] = true;
+    if(arr[0]<=k) prev[arr[0]] = true;
+    for(int i=1;i<n;i++)
     {
-        if(target==0) return true;
-        if(idx==0) return nums[0] == target;
-        if(dp[idx][target]!=-1) return dp[idx][target];
-        bool notpick = helper(idx-1,target,nums,dp);
-        bool pick = false;
-        if(nums[idx]<=target)
+        for(int tar=1;tar<=k;tar++)
         {
-            pick = helper(idx-1,target-nums[idx],nums,dp);
+            bool notpick = prev[tar];
+            bool pick = false;
+            if(arr[i]<=tar)
+            {
+                pick = prev[tar-arr[i]];
+            }
+            curr[tar] = pick | notpick;
         }
-        return dp[idx][target] = notpick | pick;
+        prev = curr;
     }
+    return prev[k];
+}
     bool canPartition(vector<int>& nums) {
         int tsum = accumulate(nums.begin(),nums.end(),0);
-        if(tsum%2!=0) return false;
+        if(tsum%2) return false;
         int sum = tsum/2;
-        int n = nums.size();
-        vector<vector<int>>dp(n,vector<int>(sum+1,-1));
-        return helper(n-1,sum,nums,dp);
+       int n = nums.size();
+        return subsetSumToK(n,sum,nums);
     }
 };
