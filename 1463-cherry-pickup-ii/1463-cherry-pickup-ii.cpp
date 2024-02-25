25a4ab9d91dd54afb9d6f147ee18f2adb02f9e8f
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-02-25 13:04:20 +0530

    Time: 62 ms (21.92%), Space: 11.6 MB (98.25%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,57 +1,20 @@
 class Solution {
 public:
-    int helper(int i,int j1,int j2,vector<vector<int>>& grid,vector<vector<vector<int>>>& dp)
-    {
-        if(j1<0 || j1>=grid[0].size() || j2<0 || j2>=grid[0].size())
-        {
-            return -1e9;
-        }
-        if(i==grid.size()-1)
-        {
-            if(j1==j2)
-            {
-                return grid[i][j1];
-            }
-            else
-            {
-                return grid[i][j1]+grid[i][j2];
-            }
-        }
-        if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
-        int maxi = -1e9;
-        for(int dj1=-1;dj1<=1;dj1++)
-        {
-            for(int dj2=-1;dj2<=1;dj2++)
-            {
-                int val = 0;
-                if(j1 == j2)
-                {
-                    val+=grid[i][j1];
-                }
-                else
-                {
-                    val = grid[i][j1] + grid[i][j2];
-                }
-                val+=helper(i+1,j1+dj1,j2+dj2,grid,dp);
-                maxi = max(maxi,val);
-            }
-        }
-        return dp[i][j1][j2] = maxi;
-    }
     int cherryPickup(vector<vector<int>>& grid) {
         int n = grid.size(),m = grid[0].size();
-        int dp[n][m][m];
+        vector<vector<int>>front(m,vector<int>(m,0));
+        vector<vector<int>>curr(m,vector<int>(m,0));
         for(int j1=0;j1<m;j1++)
         {
             for(int j2=0;j2<m;j2++)
             {
                 if(j1==j2)
                 {
-                    dp[n-1][j1][j2] = grid[n-1][j1];
+                    front[j1][j2] = grid[n-1][j1];
                 }
                 else
                 {
-                    dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2];
+                    front[j1][j2] = grid[n-1][j1] + grid[n-1][j2];
                 }
             }
         }
@@ -69,15 +32,15 @@ public:
                             int val = 0;
                             if(j1==j2)
                             {
-                                val+=grid[i][j1];
+                                val = grid[i][j1];
                             }
                             else
                             {
-                                val+=grid[i][j1]+grid[i][j2];
+                                val = grid[i][j1]+grid[i][j2];
                             }
                             if(j1+dj1>=0 && j1+dj1<m && j2+dj2>=0 && j2+dj2<m)
                             {
-                                val+=dp[i+1][j1+dj1][j2+dj2];
+                                val+=front[j1+dj1][j2+dj2];
                             }
                             else
                             {
@@ -86,10 +49,11 @@ public:
                             maxi = max(maxi,val);
                         }
                     }
-                    dp[i][j1][j2] = maxi;
+                    curr[j1][j2] = maxi;
                 }
             }
+            front = curr;
         }
-        return dp[0][0][m-1];
+        return front[0][m-1];
     }
 };
