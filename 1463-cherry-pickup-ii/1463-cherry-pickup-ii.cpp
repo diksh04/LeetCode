2a4b8e6cbeb731a7577ba092a4e3fb6f00050d0c
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-02-25 12:45:59 +0530

    Time: 58 ms (24.79%), Space: 19.4 MB (37.72%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,50 +1,46 @@
 class Solution {
 public:
     int helper(int i,int j1,int j2,vector<vector<int>>& grid,vector<vector<vector<int>>>& dp)
-{
-    if(j1<0 || j1>=grid[0].size() || j2<0 || j2>=grid[0].size())
     {
-        return -1e9;
-    }
-    if(i==grid.size()-1)
-    {
-        if(j1==j2)
+        if(j1<0 || j1>=grid[0].size() || j2<0 || j2>=grid[0].size())
         {
-            return grid[grid.size()-1][j1];
+            return -1e9;
         }
-        else
+        if(i==grid.size()-1)
         {
-            return grid[grid.size()-1][j1] + grid[grid.size()-1][j2];
-        }
-    }
-    // explore all paths
-    if(dp[i][j1][j2]!=-1)
-    {
-        return dp[i][j1][j2];
-    }
-    int maxi = -1e9;
-    for(int dj1 = -1;dj1<= 1;dj1++)//alice
-    {
-        for(int dj2 = -1;dj2<= 1;dj2++)//bob
-        {
-            int value = 0;
             if(j1==j2)
             {
-                value = grid[i][j1];
+                return grid[i][j1];
             }
             else
             {
-                value = grid[i][j1] + grid[i][j2];
+                return grid[i][j1]+grid[i][j2];
             }
-            value+=helper(i+1,j1+dj1,j2+dj2,grid,dp);
-            maxi = max(maxi,value);
         }
+        if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
+        int maxi = -1e9;
+        for(int dj1=-1;dj1<=1;dj1++)
+        {
+            for(int dj2=-1;dj2<=1;dj2++)
+            {
+                int val = 0;
+                if(j1 == j2)
+                {
+                    val+=grid[i][j1];
+                }
+                else
+                {
+                    val = grid[i][j1] + grid[i][j2];
+                }
+                val+=helper(i+1,j1+dj1,j2+dj2,grid,dp);
+                maxi = max(maxi,val);
+            }
+        }
+        return dp[i][j1][j2] = maxi;
     }
-    return dp[i][j1][j2] = maxi;
-}
     int cherryPickup(vector<vector<int>>& grid) {
-        int r = grid.size(),c = grid[0].size();
-        vector<vector<vector<int>>>dp(r,vector<vector<int>>(c,vector<int>(c,-1)));
-    return helper(0,0,c-1,grid,dp);
+        int n = grid.size(),m = grid[0].size();
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
+        return helper(0,0,m-1,grid,dp);
     }
 };
