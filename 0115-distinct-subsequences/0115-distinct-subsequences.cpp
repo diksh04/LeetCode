0d4400aa20ec49cd6cf5f817ed05c098642189f0
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-02-27 00:03:02 +0530

    Time: 14 ms (85.50%), Space: 7.6 MB (89.48%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:   
-    int numDistinct(string s, string t) {
-        static int MOD = (int)1e9 + 7; 
-        int n = s.size();
-        int m = t.size();
-        vector<vector<unsigned long long>>dp(n+1,vector<unsigned long long>(m+1,0));
-        for(int i=0;i<=n;i++)//when j reaches 0 we matched string
-        {
-            dp[i][0] = 1;
+    int numDistinct(string str, string sub) {
+        int n = str.size(), m = sub.size();
+      vector<double>prev(m + 1, 0),curr(m+1,0);
+      prev[0] = curr[0] = 1;
+      for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+          if (str[i-1] == sub[j-1]) {
+                 curr[j] = prev[j - 1] + prev[j];
+          } else {
+            curr[j] = prev [j];
+          }
         }
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=m;j++)
-            {
-                if(s[i-1]==t[j-1])
-                {
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-                }
-                else
-                {
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
-        return dp[n][m] > INT_MAX ? -1:dp[n][m]; 
+        prev = curr;
+      }
+      return (int)prev[m];
     }
 };
