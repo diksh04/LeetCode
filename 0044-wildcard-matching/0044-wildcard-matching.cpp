d8b32c1e2bf8b43f9508856c1d9dbf7a696e8a0a
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-02-27 12:56:45 +0530

    Time: 27 ms (85.25%), Space: 31.3 MB (17.81%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -2,14 +2,8 @@ class Solution {
 public:
     bool helper(int i,int j,string &s,string &p,vector<vector<int>>& dp)
     {
-        if(i<0 && j<0)
-        {
-            return true;
-        }
-        if(j<0 && i>=0)
-        {
-            return false;
-        }
+        if(i<0 && j<0) return true;
+        if(i>=0 && j<0) return false;
         if(i<0 && j>=0)
         {
             for(int ii=0;ii<=j;ii++)
@@ -21,17 +15,16 @@ public:
             }
             return true;
         }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        if(s[i]==p[j] || p[j]=='?')
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s[i]==p[j] || p[j]=='?') 
         {
             return dp[i][j] = helper(i-1,j-1,s,p,dp);
         }
         else if(p[j]=='*')
         {
-            return dp[i][j]=helper(i-1,j,s,p,dp) | helper(i,j-1,s,p,dp);
+            bool val1 = helper(i,j-1,s,p,dp);//we are not matching anything with *
+            bool val2 = helper(i-1,j,s,p,dp);//we are matching char in s with *  in p
+            return dp[i][j] = val1 | val2;
         }
         return dp[i][j] = false;
     }
