e2621b72ab0828d108a2732f0cc85669b8fa81da
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-02-27 15:54:05 +0530

    Time: 131 ms (72.99%), Space: 77.9 MB (77.75%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -4,7 +4,8 @@ public:
     int maxProfit(vector<int>& prices) {
         // dp[n][buy->0/1][cap->0/1/2]  
         int n = prices.size();
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        vector<vector<int>>ahead(2,vector<int>(3,0));
+        vector<vector<int>>curr(2,vector<int>(3,0));
         for(int idx=n-1;idx>=0;idx--)
         {
             for(int buy=0;buy<=1;buy++)
@@ -13,20 +14,21 @@ public:
                 {
                     int profit = 0;
                     if(buy){
-                        int webuy = -prices[idx] + dp[idx+1][0][cap];
-                        int notbuy = 0 + dp[idx+1][1][cap];
+                        int webuy = -prices[idx] + ahead[0][cap];
+                        int notbuy = 0 + ahead[1][cap];
                         profit = max(webuy,notbuy);
                     }
                     else
                     {
-                        int wesell = prices[idx] + dp[idx+1][1][cap-1];//transaction is completed therefore we sell
-                        int notsell = 0 + dp[idx+1][0][cap];
+                        int wesell = prices[idx] + ahead[1][cap-1];//transaction is completed therefore we sell
+                        int notsell = 0 + ahead[0][cap];
                         profit = max(wesell,notsell);
                     }
-                    dp[idx][buy][cap] = profit;
+                    curr[buy][cap] = profit;
                 }
             }
+            ahead = curr;
         }
-        return dp[0][1][2];
+        return ahead[1][2];
     }
 };
