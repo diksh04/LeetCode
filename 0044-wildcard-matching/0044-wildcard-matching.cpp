4f8d98cde0d54b3f249f50c00ed2b5a36f1848e8
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-02-27 13:11:04 +0530

    Time: 55 ms (48.28%), Space: 11.2 MB (85.68%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -30,12 +30,8 @@ public:
     }
     bool isMatch(string s, string p) {
         int n = s.size(),m = p.size();
-        vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
-        dp[0][0] = true;
-        for(int i=1;i<=n;i++)
-        {
-            dp[i][0] = false;
-        }
+        vector<bool>prev(m+1,false),curr(m+1,false);
+        prev[0] = true;
         for(int j=1;j<=m;j++)
         {
             bool flag = true;
@@ -47,25 +43,27 @@ public:
                     break;
                 }
             }
-            dp[0][j] = flag;
+            prev[j] = flag;
         }
         for(int i=1;i<=n;i++)
         {
+            curr[0] = false;
             for(int j=1;j<=m;j++)
             {
                     if(s[i-1]==p[j-1] || p[j-1]=='?') 
                     {
-                        dp[i][j] = dp[i-1][j-1];
+                        curr[j] = prev[j-1];
                     }
                     else if(p[j-1]=='*')
                     {
-                        bool val1 = dp[i][j-1];//we are not matching anything with *
-                        bool val2 = dp[i-1][j];//we are matching char in s with *  in p
-                        dp[i][j] = val1 | val2;
+                        bool val1 = curr[j-1];//we are not matching anything with *
+                        bool val2 = prev[j];//we are matching char in s with *  in p
+                        curr[j] = val1 | val2;
                     }
-                    else dp[i][j] = false;
+                    else curr[j] = false;
             }
+            prev = curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
