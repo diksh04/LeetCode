b1e0a8d5274a304076e4c44884563acd4ac11902
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-02-27 13:03:54 +0530

    Time: 24 ms (88.64%), Space: 31.5 MB (13.15%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     bool helper(int i,int j,string &s,string &p,vector<vector<int>>& dp)
     {
-        if(i<0 && j<0) return true;
-        if(i>=0 && j<0) return false;
-        if(i<0 && j>=0)
+        if(i==0 && j==0) return true;
+        if(i>0 && j==0) return false;
+        if(i==0 && j>0)
         {
-            for(int ii=0;ii<=j;ii++)
+            for(int ii=1;ii<=j;ii++)
             {
-                if(p[ii]!='*')
+                if(p[ii-1]!='*')
                 {
                     return false;
                 }
@@ -16,11 +16,11 @@ public:
             return true;
         }
         if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==p[j] || p[j]=='?') 
+        if(s[i-1]==p[j-1] || p[j-1]=='?') 
         {
             return dp[i][j] = helper(i-1,j-1,s,p,dp);
         }
-        else if(p[j]=='*')
+        else if(p[j-1]=='*')
         {
             bool val1 = helper(i,j-1,s,p,dp);//we are not matching anything with *
             bool val2 = helper(i-1,j,s,p,dp);//we are matching char in s with *  in p
@@ -30,7 +30,7 @@ public:
     }
     bool isMatch(string s, string p) {
         int n = s.size(),m = p.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return helper(n-1,m-1,s,p,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return helper(n,m,s,p,dp);
     }
 };
