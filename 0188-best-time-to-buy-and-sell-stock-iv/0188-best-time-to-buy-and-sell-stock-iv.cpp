1665b8103ed714774eb4c5895b4779425fe1893c
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-02-27 22:42:38 +0530

    Time: 11 ms (60.08%), Space: 16.5 MB (19.05%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    int helper(int idx,int buy,int cap,vector<int>& prices,vector<vector<vector<int>>>& dp)
-    {
-        if(idx==prices.size()) return 0;
-        if(cap==0) return 0;
-        if(dp[idx][buy][cap]!=-1) return dp[idx][buy][cap];
-        int profit=0;
-        if(buy){
-            int webuy = -prices[idx] + helper(idx+1,0,cap,prices,dp);
-            int notbuy = 0 + helper(idx+1,1,cap,prices,dp);
-            profit = max(webuy,notbuy);
-        }
-        else
-        {
-            int wesell = prices[idx] + helper(idx+1,1,cap-1,prices,dp);
-            int notsell = 0 + helper(idx+1,0,cap,prices,dp);
-            profit = max(wesell,notsell);
-        }
-        return dp[idx][buy][cap] = profit;
-    }
+    
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(k+1,-1)));
-        return helper(0,1,k,prices,dp);
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(k+1,0)));
+        for(int idx=n-1;idx>=0;idx--)
+        {
+            for(int buy=0;buy<=1;buy++)
+            {
+                for(int cap=1;cap<=k;cap++)
+                {
+                    int profit=0;
+                    if(buy){
+                        int webuy = -prices[idx] + dp[idx+1][0][cap];
+                        int notbuy = 0 + dp[idx+1][1][cap];
+                        profit = max(webuy,notbuy);
+                    }
+                    else
+                    {
+                        int wesell = prices[idx] + dp[idx+1][1][cap-1];
+                        int notsell = 0 + dp[idx+1][0][cap];
+                        profit = max(wesell,notsell);
+                    }
+                    dp[idx][buy][cap] = profit;
+                }
+            }
+        }
+        return dp[0][1][k];
     }
 };
