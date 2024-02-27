f381b11f3102bb68b8aea14fa17284d5ad487e92
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-02-28 00:51:49 +0530

    Time: 537 ms (16.18%), Space: 294.7 MB (5.53%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
+    int helper(int idx,int prevIdx,vector<int>& nums,vector<vector<int>>& dp)
+    {
+        if(idx==nums.size()) return 0;
+        if(dp[idx][prevIdx+1]!=-1) return dp[idx][prevIdx+1];
+        int notTake = 0 + helper(idx+1,prevIdx,nums,dp);
+        int take = 0;
+        if(prevIdx==-1 || nums[idx]>nums[prevIdx])
+        {
+            take = 1 + helper(idx+1,idx,nums,dp);
+        }
+        return dp[idx][prevIdx+1] = max(take,notTake);
+    }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-    for(int idx=n-1;idx>=0;idx--)
-    {
-        for(int prev_idx=idx-1;prev_idx>=-1;prev_idx--)
-        {
-            int len = 0 + dp[idx+1][prev_idx+1];
-            if(prev_idx == -1 || nums[idx] > nums[prev_idx])
-            {
-                len = max(len,1+dp[idx+1][idx+1]);
-            }
-            dp[idx][prev_idx+1] = len;
-        }
-    }
-    return dp[0][-1+1];
+        //we can't store at -1 we will coordinate shift         
+        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        return helper(0,-1,nums,dp);
     }
 };
