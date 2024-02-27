2b6f40f88436b6ab0f59bb57d9bf885b7f1d4784
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-02-27 23:35:21 +0530

    Time: 4 ms (57.41%), Space: 14.1 MB (34.76%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -5,15 +5,11 @@ public:
         vector<vector<int>>dp(n+2,vector<int>(2,0));
         for(int idx=n-1;idx>=0;idx--)
         {
-            for(int buy=0;buy<=1;buy++)
-            {
-                if(buy){
-            dp[idx][buy] = max(-prices[idx]+dp[idx+1][0],
+            dp[idx][1] = max(-prices[idx]+dp[idx+1][0],
                                      dp[idx+1][1]);
-                }
-        else dp[idx][buy] = max(prices[idx]+dp[idx+2][1],
+            
+            dp[idx][0] = max(prices[idx]+dp[idx+2][1],
                                  dp[idx+1][0]);
-            }
         }
         return dp[0][1];
     }
