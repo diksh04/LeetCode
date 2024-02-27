78d4180ed534f8d32599b69042bdc1df44a97c56
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-02-27 13:09:22 +0530

    Time: 80 ms (24.38%), Space: 15.7 MB (58.80%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -30,7 +30,42 @@ public:
     }
     bool isMatch(string s, string p) {
         int n = s.size(),m = p.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return helper(n,m,s,p,dp);
+        vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
+        dp[0][0] = true;
+        for(int i=1;i<=n;i++)
+        {
+            dp[i][0] = false;
+        }
+        for(int j=1;j<=m;j++)
+        {
+            bool flag = true;
+            for(int ii=1;ii<=j;ii++)
+            {
+                if(p[ii-1]!='*')
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            dp[0][j] = flag;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                    if(s[i-1]==p[j-1] || p[j-1]=='?') 
+                    {
+                        dp[i][j] = dp[i-1][j-1];
+                    }
+                    else if(p[j-1]=='*')
+                    {
+                        bool val1 = dp[i][j-1];//we are not matching anything with *
+                        bool val2 = dp[i-1][j];//we are matching char in s with *  in p
+                        dp[i][j] = val1 | val2;
+                    }
+                    else dp[i][j] = false;
+            }
+        }
+        return dp[n][m];
     }
 };
