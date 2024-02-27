6bf12c890222d1a75c29991c0ec56cad2432e4b8
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-02-27 22:58:16 +0530

    Time: 14 ms (47.44%), Space: 15.9 MB (37.12%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
-    
+    int helper(int idx,int transNo,int k,vector<int>& prices,vector<vector<int>>& dp)
+    {
+        if(idx==prices.size() || transNo == 2*k)
+        {
+            return 0;
+        }
+        if(dp[idx][transNo]!=-1) return dp[idx][transNo];
+        int profit = 0;
+        if(transNo%2==0)//buy
+        {
+            return dp[idx][transNo] = max(-prices[idx]+helper(idx+1,transNo+1,k,prices,dp),
+                      0 + helper(idx+1,transNo,k,prices,dp));
+        }
+        else
+            return dp[idx][transNo] = max(prices[idx]+helper(idx+1,transNo+1,k,prices,dp),
+                      0 + helper(idx+1,transNo,k,prices,dp));
+    }
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>>ahead(2,vector<int>(k+1,0));
-        vector<vector<int>>curr(2,vector<int>(k+1,0));
-        for(int idx=n-1;idx>=0;idx--)
-        {
-            for(int buy=0;buy<=1;buy++)
-            {
-                for(int cap=1;cap<=k;cap++)
-                {
-                    int profit=0;
-                    if(buy){
-                        int webuy = -prices[idx] + ahead[0][cap];
-                        int notbuy = 0 + ahead[1][cap];
-                        profit = max(webuy,notbuy);
-                    }
-                    else
-                    {
-                        int wesell = prices[idx] + ahead[1][cap-1];
-                        int notsell = 0 + ahead[0][cap];
-                        profit = max(wesell,notsell);
-                    }
-                    curr[buy][cap] = profit;
-                }
-            }
-            ahead = curr;
-        }
-        return ahead[1][k];
+        vector<vector<int>>dp(n+1,vector<int>(2*k+1,-1));
+        return helper(0,0,k,prices,dp);
     }
 };
