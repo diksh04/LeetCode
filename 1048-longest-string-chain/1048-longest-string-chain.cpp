b5f4f68e9dec2953d1b837d23e4ddbcdfa0de1d7
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-02-29 12:37:55 +0530

    Time: 45 ms (93.91%), Space: 16.7 MB (80.84%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,53 +1,44 @@
 class Solution {
 public:
-    bool check(string &s1,string  &s2) //s1 > s2
-    {
-        if(s1.size()!=s2.size()+1)
-        {
-            return false;
-        }
-        int first = 0,second = 0;
-        while(first<s1.size())
-        {
-            if(s1[first]==s2[second])
-            {
-                first++;
-                second++;
-            }
-            else
-            {
-                first++;
-            }
-        }
-        if(first == s1.size() && second == s2.size())
-        {
-            return true;
-        }
-        return false;
-    }
-    static bool cmp(string &s1,string &s2)
+    bool static compare(string &s1,string &s2)
     {
         return s1.size() < s2.size();
     }
+    bool checkPossible(string &s1,string &s2)
+    {
+        if(s1.size()!=s2.size()+1) return false;
+        int i=0,j=0;
+        while(i<s1.size())
+        {
+            if(j<s2.size() && s1[i]==s2[j])
+            {
+                i++;
+                j++;
+            }
+            else
+            { 
+                //abca abc
+                i++;
+            }
+        }
+        if(i==s1.size() && j==s2.size()) return true;
+        return false;
+    }
     int longestStrChain(vector<string>& words) {
+        sort(words.begin(),words.end(),compare);
         int n = words.size();
-        sort(words.begin(),words.end(),cmp);
         vector<int>dp(n,1);
         int maxi = 1;
-        int lastIdx = 0;
-        for(int idx=0;idx<n;idx++)
+        for(int idx=1;idx<n;idx++)
         {
             for(int prev=0;prev<idx;prev++)
             {
-                if(check(words[idx],words[prev]) && 1+dp[prev] > dp[idx])
+                if(checkPossible(words[idx],words[prev]) && dp[idx] < 1 + dp[prev])
                 {
-                    dp[idx] = 1+dp[prev];
+                    dp[idx] = 1 + dp[prev];
                 }
             }
-            if(dp[idx]>maxi)
-            {
-                maxi = dp[idx];
-            }
+            maxi = max(maxi,dp[idx]);
         }
         return maxi;
     }
