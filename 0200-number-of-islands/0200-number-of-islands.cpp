5820f45cb1d5444bd610a41d5f7a731a0be26c5e
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-03-02 11:47:22 +0530

    Time: 33 ms (36.78%), Space: 16.2 MB (48.86%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,45 +1,33 @@
 class Solution {
 public:
-    
-    void bfs(int i,int j,vector<vector<int>>& vis,vector<vector<char>>& grid)
+    void dfs(int r,int c,vector<vector<char>>& grid,vector<vector<bool>>& vis)
     {
-        queue<pair<int,int>>q;
-        q.push({i,j});
-        vis[i][j]=1;
-        int dRow[] = {-1,0,1,0};
-        int dCol[] = {0,1,0,-1};
-        while(!q.empty())
+        if(r<0 || r>=grid.size() || c<0 || c>=grid[0].size() || grid[r][c]=='0' || vis[r][c]==true)
         {
-            int r = q.front().first;
-            int c = q.front().second;
-            q.pop();
-            for(int k=0;k<4;k++)
-            {
-                int nrow = r + dRow[k];
-                int ncol = c + dCol[k];
-                if(nrow>=0 && nrow<grid.size() && ncol>=0 && ncol<grid[0].size() && grid[nrow][ncol]=='1' && !vis[nrow][ncol])
-                {
-                    q.push({nrow,ncol});
-                    vis[nrow][ncol]=1;
-                }
-            }
+            return;
         }
+        vis[r][c] = true;
+        dfs(r-1,c,grid,vis);
+        dfs(r,c-1,grid,vis);
+        dfs(r+1,c,grid,vis);
+        dfs(r,c+1,grid,vis);
     }
     int numIslands(vector<vector<char>>& grid) {
-        int countIsl=0;
-        int n = grid.size(),m = grid[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        for(int i=0;i<n;i++)
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<bool>>vis(m,vector<bool>(n,false));
+        int cnt = 0;
+        for(int i=0;i<m;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=0;j<n;j++)
             {
-                if(grid[i][j]=='1' && !vis[i][j])
+                if(!vis[i][j] && grid[i][j]=='1')
                 {
-                    countIsl++;
-                    bfs(i,j,vis,grid);
+                    dfs(i,j,grid,vis);
+                    cnt++;
                 }
             }
         }
-        return countIsl;
+        return cnt;
     }
 };
