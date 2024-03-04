de4225cb6ea12bcbe19056c28f7736c29aa7cdab
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-03-04 14:17:36 +0530

    Time: 142 ms (51.82%), Space: 65.4 MB (16.11%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -2,40 +2,35 @@ class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int>revAdj[n];
+        vector<vector<int>>adj(n);
+        //reverse to use topo as indegree so as to use outdegree of node = 0
         vector<int>indegree(n,0);
         for(int i=0;i<n;i++)
         {
-            for(auto nbr:graph[i])
+            for(auto it:graph[i])
             {
-                revAdj[nbr].push_back(i);
+                adj[it].push_back(i);
                 indegree[i]++;
             }
         }
         queue<int>q;
         for(int i=0;i<n;i++)
         {
-            if(indegree[i]==0)
-            {
-                q.push(i);
-            }
+            if(indegree[i]==0) q.push(i);
         }
-        vector<int>safeNodes;
+        vector<int>ans;
         while(!q.empty())
         {
             int node = q.front();
             q.pop();
-            safeNodes.push_back(node);
-            for(auto nbr: revAdj[node])
+            ans.push_back(node);
+            for(auto it:adj[node])
             {
-                indegree[nbr]--;
-                if(indegree[nbr]==0)
-                {
-                    q.push(nbr);
-                }
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
             }
         }
-        sort(safeNodes.begin(),safeNodes.end());
-        return safeNodes;
+        sort(ans.begin(),ans.end());
+        return ans;
     }
 };
