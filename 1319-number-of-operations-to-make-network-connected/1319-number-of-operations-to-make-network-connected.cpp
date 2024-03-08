54eea5fc0bb0abfb60422b03f0d38256d2255724
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-03-08 13:49:18 +0530

    Time: 81 ms (78.16%), Space: 45.9 MB (51.50%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,37 +1,66 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& adj,vector<bool>& visited,int src)
-    {
-        visited[src]=true;
-        for(int i:adj[src])
+    class DisjointSet {
+	   vector<int>rank,parent;
+	   public:
+	    DisjointSet(int n){
+	        rank.resize(n+1,0);
+	        parent.resize(n+1);
+	        for(int i=0;i<=n;i++)
+	        {
+	            parent[i] = i;
+	        }
+	    }
+	    int findParent(int node){
+	        if(node == parent[node]) return node;
+	        return parent[node] = findParent(parent[node]);
+	    }
+	    void unionByRank(int u,int v){
+	        int ultiU = findParent(u);
+	        int ultiV = findParent(v);
+	        if(ultiU == ultiV) return;
+	        if(rank[ultiU] > rank[ultiV])
+	        {
+	            parent[ultiV] = ultiU;
+	        }
+	        else if(rank[ultiV] > rank[ultiU])
+	        {
+	            parent[ultiU] = ultiV;
+	        }
+	        else
+	        {
+	            parent[ultiV] = ultiU;
+	            rank[ultiU]++;
+	        }
+	    }
+	};
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        DisjointSet ds(n);
+        int cntExtra = 0;
+        for(auto it:connections)
         {
-            if(visited[i]==false)
+            int u = it[0];
+            int v = it[1];
+            if(ds.findParent(u)==ds.findParent(v))
             {
-                dfs(adj,visited,i);
+                //extra edge
+                cntExtra++;
+            }
+            else
+            {
+                ds.unionByRank(u,v);
             }
         }
-    }
-    int makeConnected(int n, vector<vector<int>>& connections) {
-        if(connections.size() < n-1)
-        {
-            return -1;
-        }
-        vector<vector<int>>adj(n);
-        for(auto v: connections)
-        {
-            adj[v[0]].push_back(v[1]);
-            adj[v[1]].push_back(v[0]);
-        }
-        vector<bool>visited(n,false);
-        int components = 0;
+        int cntComp = 0;
         for(int i=0;i<n;i++)
         {
-            if(visited[i]==false)
+            if(ds.findParent(i)==i)
             {
-                dfs(adj,visited,i);
-                components++;
+                cntComp++;
             }
         }
-        return components-1;
+        int ans = cntComp-1;
+        if(cntExtra >= ans) return ans;
+        return -1;
     }
 };
