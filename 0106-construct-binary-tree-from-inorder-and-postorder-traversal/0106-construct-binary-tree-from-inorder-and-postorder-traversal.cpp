6da9bfe762297b8d390ad66a1ad7a3fa3656d782
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-03-16 14:00:03 +0530

    Time: 11 ms (67.67%), Space: 26 MB (62.04%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,31 +11,30 @@
  */
 class Solution {
 public:
-    TreeNode* helper(vector<int>& inorder,int inStart,int inEnd,vector<int>& postorder,int postStart,int postEnd,map<int,int>& mp)
+    TreeNode* helper(vector<int>& inorder,int inStart,int inEnd,
+                    vector<int>& postorder,int postStart,int postEnd,
+                    map<int,int>& mp)
     {
-        if(inStart > inEnd || postStart > postEnd)
-        {
-            return NULL;
-        }
+        if(inStart > inEnd || postStart > postEnd) return NULL;
         TreeNode* root = new TreeNode(postorder[postEnd]);
-        int inRoot = mp[postorder[postEnd]];
+        int inRoot = mp[root->val];
         int numsLeft = inRoot - inStart;
-        
-        root->left = helper(inorder,inStart,inRoot-1,postorder,postStart,postStart+numsLeft-1,mp);
-        root->right = helper(inorder,inRoot+1,inEnd,postorder,postStart+numsLeft,postEnd-1,mp);
+        root->left = helper(inorder,inStart,inRoot-1,postorder,postStart,
+                           postStart+numsLeft-1,mp);
+        root->right = helper(inorder,inRoot+1,inEnd,postorder,
+                            postStart+numsLeft,postEnd-1,mp);
         return root;
     }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        if(inorder.size()!=postorder.size())
-        {
-            return NULL;
-        }
+        int postStart = 0,postEnd = postorder.size()-1;
+        int inStart = 0,inEnd = inorder.size()-1;
         map<int,int>mp;
         for(int i=0;i<inorder.size();i++)
         {
             mp[inorder[i]] = i;
         }
-        TreeNode* root = helper(inorder,0,inorder.size()-1,postorder,0,postorder.size()-1,mp);
+        TreeNode* root = helper(inorder,inStart,inEnd,postorder,postStart,
+                               postEnd,mp);
         return root;
     }
 };
