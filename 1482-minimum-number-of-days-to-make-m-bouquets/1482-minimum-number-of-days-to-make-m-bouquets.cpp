af99348ea3655dda2ecf72c2893ea82fb028ddb6
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-03-21 15:57:28 +0530

    Time: 118 ms (39.51%), Space: 68.9 MB (62.12%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
     int minDays(vector<int>& bloomDay, int m, int k) {
-        if(bloomDay.size() < (long long)m*k)
-        {
-            return -1;
-        }
-        int low = INT_MAX,high = INT_MIN;
-        for(int i=0;i<bloomDay.size();i++)
-        {
-            low = min(low,bloomDay[i]);
-            high = max(high,bloomDay[i]);
-        }
-        int boqts;
+        if(bloomDay.size()<(long long)m*k) return -1;
+        int low = *min_element(bloomDay.begin(),bloomDay.end());
+        int high = *max_element(bloomDay.begin(),bloomDay.end());
+        
         while(low<=high)
         {
             int mid = low + (high-low)/2;
-            boqts = 0;
-            int count = 0;
+            int boqts=0,cnt = 0;
             for(int i=0;i<bloomDay.size();i++)
             {
                 if(bloomDay[i]<=mid)
                 {
-                    count++;
+                    cnt++;
                 }
                 else
                 {
-                    boqts+=(count/k);
-                    count=0;
+                    boqts+=(cnt/k);
+                    cnt = 0;
                 }
             }
-            boqts+=(count/k);
+            boqts+=(cnt/k);
             if(boqts >= m)
             {
                 high = mid-1;
