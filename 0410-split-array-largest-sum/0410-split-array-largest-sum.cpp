26a7f1a91a367662daf545ba01adfa94d3be3744
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-03-22 21:59:36 +0530

    Time: 4 ms (42.80%), Space: 9.1 MB (71.69%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
-    int helper(vector<int>& nums,int maxSum)
+    int countSplit(vector<int>& nums,int maxSum,int k)
     {
-        int split = 1,sum= 0;
-        for(auto it:nums)
+        int split = 1,sum = 0;
+        for(int i=0;i<nums.size();i++)
         {
-            if(sum+it<=maxSum)
+            if(sum+nums[i] <= maxSum)
             {
-                sum+=it;
+                sum+=nums[i];
             }
             else
             {
                 split++;
-                sum = it;
+                sum = nums[i];
             }
         }
         return split;
     }
     int splitArray(vector<int>& nums, int k) {
+        int n = nums.size();
         int low = *max_element(nums.begin(),nums.end());
         int high = accumulate(nums.begin(),nums.end(),0);
-        
         while(low<=high)
         {
             int mid = low + (high-low)/2;
-            int split = helper(nums,mid);
+            int split = countSplit(nums,mid,k);
             if(split > k)
             {
                 low = mid+1;
             }
-            else
-            {
-                high = mid-1;
-            }
+            else high = mid-1;
         }
         return low;
     }
