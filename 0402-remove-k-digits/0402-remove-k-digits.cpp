02775430785f45549b2aa1c3feb4956bab940fb0
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-03-27 13:25:42 +0530

    Time: 12 ms (45.60%), Space: 10.1 MB (35.35%) - LeetHub

---
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+//         intutiton: idea is simple if nums[i] < st.top() we pop that elem from stack
+        stack<char>st;
+        for(int i=0;i<num.size();i++)
+        {
+            while(!st.empty() && k>0 && num[i] < st.top())
+            {
+                k--;
+              st.pop();
+            }
+            st.push(num[i]);
+        }
+        while(!st.empty() && k>0)
+        {
+            k--;
+            st.pop();
+        }
+        string ans = "";
+        while(!st.empty())
+        {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        int j = 0;
+        while(ans[j]=='0' && ans.size()>1)
+        {
+            j++;
+        }
+        if(j!=0 && j<ans.size())
+        {
+            return ans.substr(j);
+        }
+        if(j==ans.size())
+        {
+            return "0";
+        }
+        if(ans=="") return "0";
+        return ans;
+    }
+};
