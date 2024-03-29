b2c132fa3e156af66d3d0b0f7123919a260e249d
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-03-29 13:43:43 +0530

    Time: 76 ms (42.76%), Space: 85.6 MB (99.52%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -10,31 +10,24 @@ public:
             }
             else if(bills[i]==10)
             {
-                if(five==0)
-                {
-                    return false;
-                }
-                five--;
+                if(five == 0) return false;
                 ten++;
+                five--;
             }
             else
             {
-                if(five > 0 && ten > 0)
+                if(five > 0 && ten > 0) 
                 {
                     five--;
                     ten--;
                 }
-                else if(five>=3)
+                else if(five>=3) 
                 {
                     five-=3;
                 }
-                else
-                {
-                    return false;
-                }
+                else return false;
             }
         }
         return true;
     }
 };
-// 5 
