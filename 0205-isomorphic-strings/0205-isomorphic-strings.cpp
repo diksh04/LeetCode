7f80fca3e5d28cf9828029773551830d14414c7a
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-04-02 11:56:35 +0530

    Time: 9 ms (28.10%), Space: 9.3 MB (63.23%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if(s.size()!=t.size()) return false;
         map<char,char>mp1;
         map<char,bool>mp2;
         for(int i=0;i<s.size();i++)
@@ -10,7 +9,7 @@ public:
             char ch2 = t[i];
             if(mp1.contains(ch1))
             {
-                if(mp1[ch1] != ch2)
+                if(mp1[ch1]!=ch2)
                 {
                     return false;
                 }
