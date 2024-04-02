7cbdf1716703c25333830897d6834737d1a558d3
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-04-02 14:11:10 +0530

    Time: 11 ms (12.16%), Space: 16.1 MB (32.24%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,8 +3,8 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<bool>>vis(m,vector<bool>(n,false));
-        queue<pair<pair<int,int>,int>>q;//{{i,j},t}
+        queue<pair<pair<int,int>,int>>q;
+        vector<vector<int>>vis(m,vector<int>(n,0));
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
@@ -12,13 +12,13 @@ public:
                 if(grid[i][j]==2)
                 {
                     q.push({{i,j},0});
-                    vis[i][j] = true;
+                    vis[i][j] = 1;
                 }
             }
         }
         int time = 0;
-        int dRow[4] = {-1,0,1,0};
-        int dCol[4] = {0,-1,0,1};
+        int dx[4] = {-1,0,1,0};
+        int dy[4] = {0,-1,0,1};
         while(!q.empty())
         {
             auto it = q.front();
@@ -26,16 +26,15 @@ public:
             int r = it.first.first;
             int c = it.first.second;
             int t = it.second;
-            time = max(time,t);
-            for(int i=0;i<4;i++)
+            time = max(t,time);
+            for(int k=0;k<4;k++)
             {
-                int nRow = r + dRow[i];
-                int nCol = c + dCol[i];
-                if(nRow>=0 && nRow<m && nCol>=0 && nCol<n && !vis[nRow][nCol]
-                  && grid[nRow][nCol]==1)
+                int nrow = r + dx[k];
+                int ncol = c + dy[k];
+                if(nrow>=0 && nrow < m && ncol>=0 && ncol<n && !vis[nrow][ncol] && grid[nrow][ncol]==1)
                 {
-                    q.push({{nRow,nCol},t+1});
-                    vis[nRow][nCol] = true;
+                    q.push({{nrow,ncol},t+1});
+                    vis[nrow][ncol]=1;
                 }
             }
         }
@@ -43,7 +42,7 @@ public:
         {
             for(int j=0;j<n;j++)
             {
-                if(grid[i][j]==1 && !vis[i][j])
+                if(!vis[i][j] && grid[i][j]==1)
                 {
                     return -1;
                 }
