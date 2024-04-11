743c0a920b905e84b5212c56f245940bf8be655d
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-04-11 17:04:56 +0530

    Time: 9 ms (87.48%), Space: 12.4 MB (73.87%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
         int n = grid.size();
-        vector<vector<int>>dist(n,vector<int>(n,1e8));
         priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,
         greater<pair<int,pair<int,int>>>>pq;
-        pq.push({grid[0][0],{0,0}});
+        vector<vector<int>>dist(n,vector<int>(n,1e8));
         dist[0][0] = grid[0][0];
+        pq.push({grid[0][0],{0,0}});
         int dx[4] = {-1,0,1,0};
         int dy[4] = {0,-1,0,1};
         while(!pq.empty())
         {
             auto it = pq.top();
-            pq.pop();
             int time = it.first;
             int r = it.second.first;
             int c = it.second.second;
-            if(r == n-1 && c == n-1 ) return time;
+            pq.pop();
+            if(r==n-1 && c==n-1) return time;
             for(int k=0;k<4;k++)
             {
                 int nrow = r + dx[k];
@@ -29,6 +29,7 @@ public:
                         newTime+=time;
                     }
                     else newTime = time;
+                    
                     if(dist[nrow][ncol] > newTime)
                     {
                         dist[nrow][ncol] = newTime;
