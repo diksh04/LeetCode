9c3c9f93fbe8631fba0c1b8edbb95f0ac6392c9a
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-04-13 01:04:19 +0530

    Time: 3 ms (88.65%), Space: 14.1 MB (41.68%) - LeetHub

---
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(!head || !head->next) return head;
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* prev = dummy;
+        ListNode* curr = head;
+        while(curr!=NULL && curr->next!=NULL)
+        {
+            if(curr->val!=curr->next->val)
+            {
+                prev->next = curr;
+                curr = curr->next;
+                prev = prev->next;
+            }
+            else
+            {
+                while(curr!=NULL && curr->next!=NULL && curr->val == curr->next->val)
+                {
+                    ListNode* duplicate = curr;
+                    curr = curr->next;
+                    delete duplicate;
+                }
+                curr = curr->next;
+                prev->next = curr;
+
+            }
+        }
+        return dummy->next;
+    }
+};
+
+//   1->2->3->3->4->4->5
+//      p              c
+//   1->1
+//p     c
