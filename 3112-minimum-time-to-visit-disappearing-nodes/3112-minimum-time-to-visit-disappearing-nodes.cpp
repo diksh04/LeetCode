a8e9e1bc390fa9e32d4f1ed8522dbb9fab5f091f
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-04-14 01:24:23 +0530

    Time: 395 ms (72.73%), Space: 176.4 MB (100.00%) - LeetHub

---
+++ 3112-minimum-time-to-visit-disappearing-nodes/3112-minimum-time-to-visit-disappearing-nodes.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> minimumTime(int n, vector<vector<int>>& edges, vector<int>& disappear) {
+        vector<vector<pair<int,int>>>adj(n);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        for(auto it:edges)
+        {
+            int u = it[0];
+            int v = it[1];
+            int wt = it[2];
+            adj[u].push_back({v,wt});
+            adj[v].push_back({u,wt});
+        }
+        vector<int>dist(n,1e9);
+        dist[0] = 0;
+        pq.push({0,0});
+        while(!pq.empty())
+        {
+            auto it = pq.top();
+            int dis = it.first;
+            int node = it.second;
+            pq.pop();
+            if(dis > dist[node]) continue;
+            for(auto nbr:adj[node])
+            {
+                int edW = nbr.second;
+                int adjNode = nbr.first;
+                if(disappear[adjNode] > edW + dis && dist[adjNode] > edW + dis)
+                {
+                    pq.push({edW+dis,adjNode});
+                    dist[adjNode] = edW + dis;
+                }
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(dist[i] == 1e9) dist[i] = -1;
+        }
+        return dist;
+    }
+};
