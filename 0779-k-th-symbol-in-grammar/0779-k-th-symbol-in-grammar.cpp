07af6e7c890021ec9b3ed62f04b11b0be30e58b9
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-04-15 23:36:33 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (43.68%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    int helper(int n,int k){
-        if(n==1 && k==1){
-            return 0;
-        }
+    int helper(int n,int k)
+    {
+        if(n==1 && k==1) return 0;
         int mid = (pow(2,n-1))/2;
-        if(k<=mid){
+        if(k<=mid)
+        {
             return helper(n-1,k);
         }
         return !helper(n-1,k-mid);
