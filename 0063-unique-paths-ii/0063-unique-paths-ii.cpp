8029c45a99b26e2c8a09f99e70f8a644c94af5f7
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-05-14 01:01:01 +0530

    Time: 0 ms (100.00%), Space: 10.2 MB (32.77%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,49 +1,18 @@
 class Solution {
 public:
-    int helper(int i,int j,vector<vector<int>>& obstacleGrid,vector<vector<int>>& dp)
+    int helper(int i,int j,vector<vector<int>>& arr,vector<vector<int>>& dp)
     {
-        if(i<0 || j<0 || obstacleGrid[i][j]==1)
-        {
-            return 0;
-        }
-        if(i==0 && j==0)
-        {
-            return 1;
-        }
+        if(i < 0 || j < 0 || arr[i][j]==1) return 0;
+        if(i==0 && j==0) return 1;
         if(dp[i][j]!=-1) return dp[i][j];
-        int left = helper(i,j-1,obstacleGrid,dp);
-        int top = helper(i-1,j,obstacleGrid,dp);
-        return dp[i][j] = left + top;
+        int left = helper(i-1,j,arr,dp);
+        int right = helper(i,j-1,arr,dp);
+        return dp[i][j] = left + right;
     }
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m = obstacleGrid.size(),n = obstacleGrid[0].size();
-        vector<int>prev(n,0);
-        for(int i=0;i<m;i++)
-        {
-            vector<int>curr(n,0);
-            for(int j=0;j<n;j++)
-            {
-                if(obstacleGrid[i][j]==1) curr[j] = 0;
-                else if(i==0 && j==0)
-                {
-                    curr[j] = 1;
-                }
-                else
-                {
-                    int left = 0,top = 0;
-                    if(i-1>=0)
-                    {
-                        top = prev[j]; 
-                    }
-                    if(j-1>=0)
-                    {
-                        left = curr[j-1];
-                    }
-                    curr[j] = left + top;
-                }
-            }
-            prev = curr;
-        }
-        return prev[n-1];
+    int uniquePathsWithObstacles(vector<vector<int>>& arr) {
+        int m = arr.size();
+        int n = arr[0].size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        return helper(m-1,n-1,arr,dp);
     }
 };
