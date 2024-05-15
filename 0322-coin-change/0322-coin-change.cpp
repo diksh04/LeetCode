958d1cdad89bce40136fe36d666d340c6a5a8330
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-05-16 00:32:31 +0530

    Time: 41 ms (37.58%), Space: 47.3 MB (22.90%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
+    int helper(int idx,vector<int>& coins,int amount,vector<vector<int>>& dp)
+    {
+        if(idx==0)
+        {
+            if(amount%coins[0] == 0) return amount/coins[0];
+            else return 1e9;
+        }
+        if(dp[idx][amount]!=-1) return dp[idx][amount];
+        int notpick = 0 + helper(idx-1,coins,amount,dp);
+        int pick = 1e9;
+        if(coins[idx]<=amount)
+        {
+            pick = 1 + helper(idx,coins,amount-coins[idx],dp);
+        }
+        return dp[idx][amount] = min(pick,notpick);
+    }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<int>prev(amount+1,0),curr(amount+1,0);
-        for(int t=0;t<=amount;t++)
-        {
-            if(t%coins[0]==0)
-            {
-                prev[t] = t/coins[0];
-            }
-            else
-            {
-                prev[t] = 1e9;
-            }
-        }
-        for(int i=1;i<n;i++)
-        {
-            for(int t=0;t<=amount;t++)
-            {
-                int notpick = 0 + prev[t];
-                int pick = 1e9;
-                if(coins[i]<=t)
-                {
-                    pick = 1 + curr[t-coins[i]];
-                }
-                curr[t] = min(pick,notpick);
-            }
-            prev = curr;
-        }
-        if(prev[amount]>=1e9) return -1;
-        return prev[amount];
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        int ans = helper(n-1,coins,amount,dp);
+        if(ans >= 1e9) return -1;
+        return ans;
     }
 };
