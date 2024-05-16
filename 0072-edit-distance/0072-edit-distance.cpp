63a0c952346992ffdce5d34af8557e63f0273abc
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-05-16 15:52:26 +0530

    Time: 48 ms (15.03%), Space: 80.8 MB (11.92%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    int helper(int i,int j,string &word1,string &word2,vector<vector<int>>& dp)
+    int helper(int i,string word1,int j,string word2,vector<vector<int>>& dp)
     {
-        if(i<0) return j+1;
-        if(j<0) return i+1;
-        if(dp[i][j]!=-1)
+        if(i<0) return j+1;//word1 is exhausted,insert all chars of word2
+        if(j<0) return i+1;//word2 is exhuasted,delete all chars of word1
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(word1[i] == word2[j])
         {
-            return dp[i][j];
-        }
-        if(word1[i]==word2[j])
-        {
-            return dp[i][j] = helper(i-1,j-1,word1,word2,dp);
+            return dp[i][j] = helper(i-1,word1,j-1,word2,dp);
         }
         else
         {
-            int insertion  = 1 + helper(i,j-1,word1,word2,dp);
-            int deletion = 1 + helper(i-1,j,word1,word2,dp);
-            int replace = 1 + helper(i-1,j-1,word1,word2,dp);
+            int insertion = 1 + helper(i,word1,j-1,word2,dp);
+            int deletion =  1 + helper(i-1,word1,j,word2,dp);
+            int replace = 1 + helper(i-1,word1,j-1,word2,dp);
             return dp[i][j] = min({insertion,deletion,replace});
         }
     }
     int minDistance(string word1, string word2) {
         int n = word1.size(),m = word2.size();
         vector<vector<int>>dp(n,vector<int>(m,-1));
-        return helper(n-1,m-1,word1,word2,dp);
+        return helper(n-1,word1,m-1,word2,dp);
     }
 };
