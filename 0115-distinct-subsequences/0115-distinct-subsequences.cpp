c44a6407252bf6ec87d9a4e50317369e58c202fa
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-05-16 14:12:42 +0530

    Time: 1709 ms (12.43%), Space: 421.7 MB (6.94%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,19 +1,20 @@
 class Solution {
-public:   
-    int numDistinct(string str, string sub) {
-        int n = str.size(), m = sub.size();
-      vector<double>prev(m + 1, 0),curr(m+1,0);
-      prev[0] = curr[0] = 1;
-      for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-          if (str[i-1] == sub[j-1]) {
-                 curr[j] = prev[j - 1] + prev[j];
-          } else {
-            curr[j] = prev [j];
-          }
+public:
+    int helper(int i,int j,string s,string t,vector<vector<int>>& dp)
+    {
+        if(j==0) return 1;//t is exhausted
+        if(i==0) return 0;//s is exhausted
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s[i-1]==t[j-1])
+        {
+            return dp[i][j] = helper(i-1,j-1,s,t,dp) + helper(i-1,j,s,t,dp);
         }
-        prev = curr;
-      }
-      return (int)prev[m];
+        else return dp[i][j] = helper(i-1,j,s,t,dp);
+        
+    }
+    int numDistinct(string s, string t) {
+        int n = s.size(),m = t.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return helper(n,m,s,t,dp);
     }
 };
