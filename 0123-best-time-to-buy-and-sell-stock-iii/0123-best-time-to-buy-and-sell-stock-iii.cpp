d6c3140ba34a09a937e50dc1dd4f8c3ae2f83663
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-05-16 22:02:25 +0530

    Time: 486 ms (15.65%), Space: 222.1 MB (17.31%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
-    
+     int helper(int idx,int buy,int cap,vector<int>& prices,vector<vector<vector<int>>>& dp)
+     {
+         if(idx == prices.size()) return 0;
+         if(cap == 0) return 0;
+         if(dp[idx][buy][cap]!=-1) return dp[idx][buy][cap];
+         int profit = 0;
+         if(buy)
+         {
+             int weBuy = -prices[idx] + helper(idx+1,0,cap,prices,dp);
+             int notBuy = helper(idx+1,1,cap,prices,dp);
+             profit = max(weBuy,notBuy);
+         }
+         else
+         {
+             int weSell= prices[idx] + helper(idx+1,1,cap-1,prices,dp);
+             int notSell = helper(idx+1,0,cap,prices,dp);
+             profit = max(weSell,notSell);
+         }
+         return dp[idx][buy][cap] = profit;
+     }
     int maxProfit(vector<int>& prices) {
-        // dp[n][buy->0/1][cap->0/1/2]  
         int n = prices.size();
-        vector<vector<int>>ahead(2,vector<int>(3,0));
-        vector<vector<int>>curr(2,vector<int>(3,0));
-        for(int idx=n-1;idx>=0;idx--)
-        {
-            for(int buy=0;buy<=1;buy++)
-            {
-                for(int cap=1;cap<=2;cap++)
-                {
-                    int profit = 0;
-                    if(buy){
-                        int webuy = -prices[idx] + ahead[0][cap];
-                        int notbuy = 0 + ahead[1][cap];
-                        profit = max(webuy,notbuy);
-                    }
-                    else
-                    {
-                        int wesell = prices[idx] + ahead[1][cap-1];//transaction is completed therefore we sell
-                        int notsell = 0 + ahead[0][cap];
-                        profit = max(wesell,notsell);
-                    }
-                    curr[buy][cap] = profit;
-                }
-            }
-            ahead = curr;
-        }
-        return ahead[1][2];
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
+        return helper(0,1,2,prices,dp);
     }
 };
