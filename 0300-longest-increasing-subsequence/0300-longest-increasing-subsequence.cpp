5c80910b98dedf61221489e3d1e787f2f9fa67a1
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-05-17 00:21:18 +0530

    Time: 155 ms (31.83%), Space: 13.1 MB (36.18%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -16,20 +16,21 @@ public:
     }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+       vector<int>next(n+1,0),curr(n+1,0);
         for(int idx=n-1;idx>=0;idx--)
         {
             for(int prev=idx-1;prev>=-1;prev--)
             {
-                int notTake = 0 + dp[idx+1][prev+1];
+                int notTake = 0 + next[prev+1];
                 int take = 0;
                 if(prev == -1 || nums[idx] > nums[prev])
                 {
-                    take = 1 + dp[idx+1][idx+1];
+                    take = 1 +next[idx+1];
                 }
-                dp[idx][prev+1] = max(take,notTake);
+                curr[prev+1] = max(take,notTake);
             }
+            next = curr;
         }
-        return dp[0][0];
+        return next[0];
     }
 };
