a9eb44fb0b164ecb8cf71b3fd7508e8059beeaf7
diksh04 <84238934+diksh04@users.noreply.github.com>
2024-05-16 21:50:49 +0530

    Time: 8 ms (28.98%), Space: 20.3 MB (6.39%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
+    int helper(int idx,int buy,vector<int>& prices,vector<vector<int>>& dp)
+    {
+        if(idx==prices.size()) return 0;
+        if(dp[idx][buy]!=-1) return dp[idx][buy];
+        
+        int profit = 0;
+        if(buy)
+        {
+            int weBuy = -prices[idx] + helper(idx+1,0,prices,dp);
+            int notBuy = helper(idx+1,1,prices,dp);
+            profit = max(weBuy,notBuy);
+        }
+        else
+        {
+            int weSell = prices[idx] + helper(idx+1,1,prices,dp);
+            int notSell = helper(idx+1,0,prices,dp);
+            profit = max(weSell,notSell);
+        }
+        return dp[idx][buy] = profit;
+    }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<long>front(2,0),curr(2,0);
-        front[0] = front[1] = 0;
-        for(int idx=n-1;idx>=0;idx--)
-        {
-            for(int buy=0;buy<=1;buy++)
-            {
-                long profit = 0;
-                if(buy){
-                    //buy
-                    int webuy = -prices[idx] + front[0];
-                    int notbuy = 0 + front[1];
-                    profit = max(webuy,notbuy);
-                }
-                else
-                {
-                    //sell
-                    int wesell = prices[idx] + front[1];
-                    int notsell = 0 + front[0];
-                    profit = max(wesell,notsell);
-                }
-                curr[buy] = profit;
-            }
-            front = curr;
-        }
-        return front[1];
+        vector<vector<int>>dp(n,vector<int>(2,-1));
+        return helper(0,1,prices,dp);
     }
 };
